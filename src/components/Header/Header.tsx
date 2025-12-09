11d6aab60d3208a4da57b5d86eb1ceca3f88366d
AliusSidlauskas <alius.sidlauskas@gmail.com>
2025-12-09 14:15:29 +0200

    First upload

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./header.module.css";
+
+type LinkType = {
+  id: number;
+  title: string;
+  href: string;
+};
+
+type HeaderProps = {
+  links?: LinkType[];
+};
+
+const Header = ({ links = [] }: HeaderProps) => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <header className={styles.headerBase}>
+      <div className={styles.headerBox}>
+        <h1>WestCoastVolvo</h1>
+
+        <nav className={styles.nav}>
+          {links.map((link) => (
+            <Link key={link.id} href={link.href}>
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+
+        <button onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}>
+           <Image className={styles.burger} src="/hamburger-sidebar.svg" alt="burger" fill />
+        </button>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className={styles.mobileNav}>
+          {links.map((link) => (
+            <Link
+              key={link.id}
+              href={link.href}
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/data/links.ts
@@ -0,0 +1,5 @@
+export const links = [
+  { id: 1, title: "Skelbimai", href: "/adds" },
+  { id: 2, title: "Diskusijos", href: "/discusions" },
+  { id: 3, title: "Informacija", href: "/info" },
+];
